0a7cdac78d9f1aad35ea874bcd527e026b466de2
Eduardo Russo <russoedu@gmail.com>
2016-06-07 15:27:35 -0300

    [master] update

--- moblet/m-simple-list.js
+++ moblet/m-simple-list.js
@@ -1,18 +1,37 @@
-require('./m-simple-list.scss');
-var path = require('path'),
-  fs = require('fs');
-
-
-angular.module('uMoblets')
-  .directive('mMobletBase', function() {
-    return {
-      restrict: 'E',
-      scope: {
-        moblet: "=",
-        data: "="
-      },
-      template: fs.readFileSync(path.join(__dirname, 'm-simple-list.html'), 'utf8'),
-      link: function() {},
-      controller: function($scope, $loading, $ionicPopup, $uAlert, $filter) {}
+/* eslint no-undef: [0]*/
+module.exports = {
+  title: "mSimpleList",
+  style: "m-simple-list.scss",
+  template: 'm-simple-list.html',
+  i18n: {
+    pt: "lang/pt-BR.json",
+    en: "lang/en-US.json"
+  },
+  link: function() {},
+  controller: function(
+    $scope,
+    $uMoblet,
+    $uFeedLoader,
+    $filter,
+    $ionicScrollDelegate,
+    $uAlert,
+    $timeout
+  ) {
+    var init = function() {
+      $scope.isLoading = true;
+      $scope.moblet = $uMoblet.load();
+      var options = {
+        offset: 1,
+        items: 25,
+        itemsArray: "locations"
+      };
+      $uFeedLoader.load($scope.moblet, options, false)
+        .then(function(data) {
+          // Put the data from the feed in the $scope object
+          $scope.data = data;
+        });
     };
-  });
+
+    init();
+  }
+};
