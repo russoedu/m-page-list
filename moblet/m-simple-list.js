10ddff44d4dbdc4a04ecab1d85f15082a5bb8f5d
Eduardo Russo <russoedu@gmail.com>
2016-06-09 14:27:39 -0300

    [master] Listing items and changing url to detail. No detail shown, yet

--- moblet/m-simple-list.js
+++ moblet/m-simple-list.js
@@ -10,28 +10,111 @@ module.exports = {
   link: function() {},
   controller: function(
     $scope,
+    $rootScope,
     $mMoblet,
     $mDataLoader,
-    $filter
+    $filter,
+    $timeout,
+    $state,
+    $stateParams
   ) {
-    var init = function() {
-      $scope.isLoading = true;
-      $scope.moblet = $mMoblet.load();
-      var options = {
-        offset: 1,
-        items: 25,
-        itemsArray: "items"
-      };
-      $mDataLoader.load($scope.moblet, false, options)
-        .then(function(data) {
-          // Put the data from the feed in the $scope object
-          for (var i = 0; i < data.items.length; i++) {
-            data.items[i].description = unescape(data.items[i].description);
-          }
-          $scope.data = data;
-        });
+    var dataLoadOptions = {
+      offset: 0,
+      items: 25
     };
 
-    init();
+    var list = {
+      /**
+       * Set the view and update the needed parameters
+       * @param  {object} data Data received from Moblets backend
+       * @param  {boolean} more If called by "more" function, it will add the
+       * data to the items array
+       */
+      setView: function(data, more) {
+        if (isDefined(data)) {
+          $scope.error = false;
+          $scope.style = data.style;
+
+          // If it was called from the "more" function, concatenate the items
+          $scope.items = (more) ? $scope.items.concat(data.items) : data.items;
+
+          // Disable the "more" function if the API don't have more items
+          $scope.more = (data.hasMoreItems) ? list.more : undefined;
+        // $scope.detail = $stateParams.detail;
+        // $scope.isDetail = utils.checkDetail();
+        } else {
+          $scope.error = true;
+        }
+        $scope.isLoading = false;
+        $rootScope.$broadcast('scroll.refreshComplete');
+        $rootScope.$broadcast('scroll.infiniteScrollComplete');
+        $scope.data = data;
+      },
+      /**
+       * Load data from the Moblets backend:
+       * - show the page loader if it's called by init (sets showLoader to true)
+       * - Use $mDataLoader.load to get the moblet data from Moblets backend.
+       * 	 The parameters passed to $mDataLoader.load are:
+       * 	 - $scope.moblet - the moblet created in the init function
+       * 	 - false - A boolean that sets if you want to load data from the
+       * 	   device storage or from the Moblets API
+       * 	 - dataLoadOptions - An object with parameters for pagination
+       * @param  {boolean} showLoader Boolean to determine if the page loader
+       * is active
+       */
+      load: function(showLoader) {
+        $scope.isLoading = showLoader || false;
+        // Reset the pagination
+        dataLoadOptions.offset = 0;
+        $mDataLoader.load($scope.moblet, false, dataLoadOptions)
+          .then(function(data) {
+            list.setView(data)
+          }
+        );
+      },
+      /**
+       * Load more data from the backend if there are more items.
+       * - Update the offset summing the number of items
+       - Use $mDataLoader.load to get the moblet data from Moblets backend.
+       * 	 The parameters passed to $mDataLoader.load are:
+       * 	 - $scope.moblet - the moblet created in the init function
+       * 	 - false - A boolean that sets if you want to load data from the
+       * 	   device storage or from the Moblets API
+       * 	 - dataLoadOptions - An object with parameters for pagination
+       */
+      more: function() {
+        // Add the items to the offset
+        dataLoadOptions.offset += dataLoadOptions.items;
+        $mDataLoader.load($scope.moblet, false, dataLoadOptions)
+          .then(function(data) {
+            list.setView(data, true);
+          });
+      },
+      /**
+       * Initiate the list moblet:
+       * - Create a moblet with $mMoblet.load()
+       * - put the list.load function in the $scope
+       * - run list.load function
+       */
+      init: function() {
+        $scope.moblet = $mMoblet.load();
+        $scope.load = list.load;
+        $scope.load(true);
+      }
+    }
+
+    var item = {
+      goTo: function(id) {
+        $stateParams.detail = id;
+        console.log(id);
+        console.log($state);
+        console.log($stateParams);
+        $state.go('moblet', $stateParams);
+      }
+    };
+
+    $scope.load = list.load;
+    $scope.goTo = item.goTo;
+    list.init();
   }
 };
